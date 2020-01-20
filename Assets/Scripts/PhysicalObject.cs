14065feae4f82554c89d374d45a83fa80a4cdd6c
Reclocer <54275125+Reclocer@users.noreply.github.com>
2020-01-20 20:21:00 +0300

    add script PhysicalObject with attributes

---
+++ Assets/Scripts/PhysicalObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PhysicalObject : MonoBehaviour
+{
+    [Header("Object parameters")]
+    [SerializeField] private Color _ojectColor = Color.blue;
+    public Color ObjectColor => _ojectColor;
+
+    [SerializeField] private float _objectMass = 4.5f;
+    public float ObjectMass => _objectMass;
+
+    [HideInInspector] public const float AtmospherePressure = 0;
+
+    [Space(20)]
+    [SerializeField][Range(0, 360)] private float _rotateSpeed = 20;
+    [SerializeField] private float _moveSpeed = 3;
+
+    private Rigidbody _rigidbody;
+
+    [SerializeField] [TextArea(2, 5)] private string Specification = "Sphere";
+
+
+    void Start()
+    {
+        StartInitialization();
+        _rigidbody.velocity = new Vector3(_moveSpeed, 0, 0);
+        _rigidbody.rotation = Quaternion.Euler(_rotateSpeed, 0, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        //_rigidbody.velocity = new Vector3(_moveSpeed, 0, 0);
+        //_rigidbody.rotation = Quaternion.Euler(_rotateSpeed, 0, 0);
+    }
+
+    private void StartInitialization()
+    {
+        GetComponent<MeshRenderer>().material.color = _ojectColor;
+        _rigidbody = GetComponent<Rigidbody>();        
+        _rigidbody.mass = _objectMass;
+    }
+}
