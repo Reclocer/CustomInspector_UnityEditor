7830a0706007a659ca519115e6c219cd2e270f2d
Reclocer <54275125+Reclocer@users.noreply.github.com>
2020-01-21 23:11:24 +0300

    Add soldier with [ExecuteInEditMode]

---
+++ Assets/Scripts/TransformEdit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TransformEdit : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+        
+    void Update()
+    {
+        if(_target)
+        {
+            transform.LookAt(_target);
+        }
+    }
+}
